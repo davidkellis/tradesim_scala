b10822d8ae47b195b73b55f3e22c6dfbbc9e2fc7
David Ellis <davidkellis@gmail.com>
2013-04-01 22:35:22 -0500

    almost done porting ordering.clj to ordering.scala

--- src/main/scala/dke/tradesim/core.scala
+++ src/main/scala/dke/tradesim/core.scala
@@ -3,34 +3,37 @@ package dke.tradesim
 import org.joda.time.{DateTime}
 
 object core {
-  type StockHoldings = Map[String, Double]
-  case class Portfolio(cash: Double,
+  type StockHoldings = Map[String, Long]
+  case class Portfolio(cash: BigDecimal,
                        stocks: StockHoldings)
 
-  sealed abstract class Order {
+  sealed trait Order {
+    val time: DateTime
     val symbol: String
-    val qty: Double
-    val fillPrice: Double
-    def changeQty(newQty: Double): Order
+    val qty: Long
+    val fillPrice: Option[BigDecimal]
+    def changeQty(newQty: Long): Order
   }
-  sealed abstract class LimitOrder extends Order {
-    def changeLimitPrice(newLimitPrice: Double): LimitOrder
+  type MarketOrder = Order
+  sealed trait LimitOrder extends Order {
+    val limitPrice: BigDecimal
+    def changeLimitPrice(newLimitPrice: BigDecimal): LimitOrder
   }
-  sealed trait BuyOrder
-  sealed trait SellOrder
-  case class MarketBuy(time: DateTime, symbol: String, qty: Double, fillPrice: Double) extends Order with BuyOrder {
-    def changeQty(newQty: Double): MarketBuy = this.copy(qty = newQty)
+  sealed trait BuyOrder extends Order
+  sealed trait SellOrder extends Order
+  case class MarketBuy(time: DateTime, symbol: String, qty: Long, fillPrice: Option[BigDecimal]) extends BuyOrder {
+    def changeQty(newQty: Long): MarketBuy = this.copy(qty = newQty)
   }
-  case class MarketSell(time: DateTime, symbol: String, qty: Double, fillPrice: Double) extends Order with SellOrder {
-    def changeQty(newQty: Double): MarketSell = this.copy(qty = newQty)
+  case class MarketSell(time: DateTime, symbol: String, qty: Long, fillPrice: Option[BigDecimal]) extends SellOrder {
+    def changeQty(newQty: Long): MarketSell = this.copy(qty = newQty)
   }
-  case class LimitBuy(time: DateTime, symbol: String, qty: Double, limitPrice: Double, fillPrice: Double) extends LimitOrder with BuyOrder {
-    def changeQty(newQty: Double): LimitBuy = this.copy(qty = newQty)
-    def changeLimitPrice(newLimitPrice: Double): LimitBuy = this.copy(limitPrice = newLimitPrice)
+  case class LimitBuy(time: DateTime, symbol: String, qty: Long, limitPrice: BigDecimal, fillPrice: Option[BigDecimal]) extends LimitOrder with BuyOrder {
+    def changeQty(newQty: Long): LimitBuy = this.copy(qty = newQty)
+    def changeLimitPrice(newLimitPrice: BigDecimal): LimitBuy = this.copy(limitPrice = newLimitPrice)
   }
-  case class LimitSell(time: DateTime, symbol: String, qty: Double, limitPrice: Double, fillPrice: Double) extends LimitOrder with SellOrder {
-    def changeQty(newQty: Double): LimitSell = this.copy(qty = newQty)
-    def changeLimitPrice(newLimitPrice: Double): LimitSell = this.copy(limitPrice = newLimitPrice)
+  case class LimitSell(time: DateTime, symbol: String, qty: Long, limitPrice: BigDecimal, fillPrice: Option[BigDecimal]) extends LimitOrder with SellOrder {
+    def changeQty(newQty: Long): LimitSell = this.copy(qty = newQty)
+    def changeLimitPrice(newLimitPrice: BigDecimal): LimitSell = this.copy(limitPrice = newLimitPrice)
   }
 
   case class State(previousTime: DateTime,
@@ -40,22 +43,22 @@ object core {
                    transactions: Seq[Order])
 
   case class Trial(symbols: Seq[String],
-                   principal: Double,
-                   commissionPerTrade: Double,
-                   commissionPerShare: Double,
+                   principal: BigDecimal,
+                   commissionPerTrade: BigDecimal,
+                   commissionPerShare: BigDecimal,
                    startTime: DateTime,
                    endTime: DateTime,
                    incrementTime: (DateTime) => DateTime,
-                   purchaseFillPrice: (DateTime, String) => Double,
-                   saleFillPrice: (DateTime, String) => Double)
+                   purchaseFillPrice: (DateTime, String) => BigDecimal,
+                   saleFillPrice: (DateTime, String) => BigDecimal)
 
   case class Strategy(buildInitState: (Strategy, Trial) => State,
                       buildNextState: (Strategy, Trial, State) => State,
                       isFinalState: (Strategy, Trial, State) => Boolean)
 
-  def defaultInitialState(time: DateTime, principal: Double) = State(time,
-                                                                     time,
-                                                                     Portfolio(principal, Map[String, Double]()),
-                                                                     Vector(),
-                                                                     Vector())
+  def defaultInitialState(time: DateTime, principal: BigDecimal) = State(time,
+                                                                         time,
+                                                                         Portfolio(principal, Map[String, Long]()),
+                                                                         Vector(),
+                                                                         Vector())
 }
--- src/main/scala/dke/tradesim/ordering.scala
+++ src/main/scala/dke/tradesim/ordering.scala
@@ -6,37 +6,39 @@ import dke.tradesim.core.LimitSell
 import org.joda.time.DateTime
 
 object ordering {
+  type PriceQuoteFn = (DateTime, String) => BigDecimal
+
   def isLimitOrder(order: Order): Boolean = order.isInstanceOf[LimitBuy] || order.isInstanceOf[LimitSell]
 
-  def setOrderQty(order: Order, newQty: Double): Order = order.changeQty(newQty)
+  def setOrderQty(order: Order, newQty: Long): Order = order.changeQty(newQty)
 
-  def setLimitPrice(order: LimitOrder, newLimitPrice: Double): LimitOrder = order.changeLimitPrice(newLimitPrice)
+  def setLimitPrice(order: LimitOrder, newLimitPrice: BigDecimal): LimitOrder = order.changeLimitPrice(newLimitPrice)
 
-  def sharesOnHand(portfolio: Portfolio, symbol: String): Double = portfolio.stocks.get(symbol).getOrElse(0)
+  def sharesOnHand(portfolio: Portfolio, symbol: String): Long = portfolio.stocks.get(symbol).getOrElse(0)
 
-  def addCash(portfolio: Portfolio, amount: Double): Portfolio = portfolio.copy(cash = portfolio.cash + amount)
+  def addCash(portfolio: Portfolio, amount: BigDecimal): Portfolio = portfolio.copy(cash = portfolio.cash + amount)
 
-  def setSharesOnHand(portfolio: Portfolio, symbol: String, count: Double): Portfolio =
-    portfolio.copy(stocks = portfolio.stocks + (symbol -> count))
+  def setSharesOnHand(portfolio: Portfolio, symbol: String, qty: Long): Portfolio =
+    portfolio.copy(stocks = portfolio.stocks + (symbol -> qty))
 
-  def purchaseCost(qty: Double, price: Double, commissionPerTrade: Double, commissionPerShare: Double): Double =
+  def purchaseCost(qty: Long, price: BigDecimal, commissionPerTrade: BigDecimal, commissionPerShare: BigDecimal): BigDecimal =
     (qty * (price + commissionPerShare)) + commissionPerTrade
   def purchaseCost(time: DateTime,
                    symbol: String,
-                   qty: Double,
-                   commissionPerTrade: Double,
-                   commissionPerShare: Double,
-                   priceFn: (DateTime, String) => Double): Double =
+                   qty: Long,
+                   commissionPerTrade: BigDecimal,
+                   commissionPerShare: BigDecimal,
+                   priceFn: PriceQuoteFn): BigDecimal =
     purchaseCost(qty, priceFn(time, symbol), commissionPerTrade, commissionPerShare)
 
-  def saleProceeds(qty: Double, price: Double, commissionPerTrade: Double, commissionPerShare: Double): Double =
+  def saleProceeds(qty: Long, price: BigDecimal, commissionPerTrade: BigDecimal, commissionPerShare: BigDecimal): BigDecimal =
     (qty * (price - commissionPerShare)) + commissionPerTrade
   def saleProceeds(time: DateTime,
                    symbol: String,
-                   qty: Double,
-                   commissionPerTrade: Double,
-                   commissionPerShare: Double,
-                   priceFn: (DateTime, String) => Double): Double =
+                   qty: Long,
+                   commissionPerTrade: BigDecimal,
+                   commissionPerShare: BigDecimal,
+                   priceFn: PriceQuoteFn): BigDecimal =
     saleProceeds(qty, priceFn(time, symbol), commissionPerTrade, commissionPerShare)
 
   def adjustPortfolioFromFilledOrder(trial: Trial, portfolio: Portfolio, order: Order): Portfolio = {
@@ -44,16 +46,65 @@ object ordering {
     val commissionPerShare = trial.commissionPerShare
     val symbol = order.symbol
     val orderQty = order.qty
-    val fillPrice = order.fillPrice
+    val fillPrice = order.fillPrice.get
     val cashOnHand = portfolio.cash
     val sharesHeld = sharesOnHand(portfolio, symbol)
     order match {
       case _: BuyOrder =>   // adjust the portfolio for a purchase
-        portfolio.copy(stocks = portfolio.stocks + (symbol -> sharesHeld + orderQty),
+        portfolio.copy(stocks = portfolio.stocks + (symbol -> (sharesHeld + orderQty)),
                        cash = cashOnHand - purchaseCost(orderQty, fillPrice, commissionPerTrade, commissionPerShare))
       case _: SellOrder =>  // adjust the portfolio for a sale
-        portfolio.copy(stocks = portfolio.stocks + (symbol -> sharesHeld - orderQty),
+        portfolio.copy(stocks = portfolio.stocks + (symbol -> (sharesHeld - orderQty)),
                        cash = cashOnHand + saleProceeds(orderQty, fillPrice, commissionPerTrade, commissionPerShare))
     }
   }
+
+  def maxSharesPurchasable(trial: Trial,
+                           portfolio: Portfolio,
+                           time: DateTime,
+                           symbol: String,
+                           bestOfferPriceFn: PriceQuoteFn): BigDecimal = {
+    val principal = portfolio.cash
+    val commissionPerTrade = trial.commissionPerTrade
+    val commissionPerShare = trial.commissionPerShare
+    (principal - commissionPerTrade).quot(bestOfferPriceFn(time, symbol) + commissionPerShare)
+  }
+
+//  def isMarketBuyFillable(trial: Trial, portfolio: Portfolio, order: MarketOrder, fillPriceFn: PriceQuoteFn): Boolean =
+//    purchaseCost(order.time, order.symbol, order.qty, trial.commissionPerTrade, trial.commissionPerShare, fillPriceFn) <= portfolio.cash
+//
+//  def isMarketSellFillable(trial: Trial, portfolio: Portfolio, order: MarketOrder, fillPriceFn: PriceQuoteFn): Boolean =
+//    saleProceeds(order.time, order.symbol, order.qty, trial.commissionPerTrade, trial.commissionPerShare, fillPriceFn) >= 0.0
+//
+//  def isLimitBuyFillable(trial: Trial, portfolio: Portfolio, order: LimitOrder, fillPriceFn: PriceQuoteFn): Boolean =
+//    isMarketBuyFillable(trial, portfolio, order, fillPriceFn) && fillPriceFn(order.time, order.symbol) <= order.limitPrice
+//
+//  def isLimitSellFillable(trial: Trial, portfolio: Portfolio, order: LimitOrder, fillPriceFn: PriceQuoteFn): Boolean =
+//    isMarketSellFillable(trial, portfolio, order, fillPriceFn) && fillPriceFn(order.time, order.symbol) >= order.limitPrice
+
+  def isOrderFillable(trial: Trial, portfolio: Portfolio, order: MarketBuy, purchaseFillPriceFn: PriceQuoteFn, saleFillPriceFn: PriceQuoteFn): Boolean =
+    purchaseCost(order.time, order.symbol, order.qty, trial.commissionPerTrade, trial.commissionPerShare, purchaseFillPriceFn) <= portfolio.cash
+
+  def isOrderFillable(trial: Trial, portfolio: Portfolio, order: MarketSell, purchaseFillPriceFn: PriceQuoteFn, saleFillPriceFn: PriceQuoteFn): Boolean =
+    saleProceeds(order.time, order.symbol, order.qty, trial.commissionPerTrade, trial.commissionPerShare, saleFillPriceFn) >= 0.0
+
+  def isOrderFillable(trial: Trial, portfolio: Portfolio, order: LimitBuy, purchaseFillPriceFn: PriceQuoteFn, saleFillPriceFn: PriceQuoteFn): Boolean =
+    isOrderFillable(trial, portfolio, order.asInstanceOf[MarketBuy], purchaseFillPriceFn, saleFillPriceFn) && purchaseFillPriceFn(order.time, order.symbol) <= order.limitPrice
+
+  def isOrderFillable(trial: Trial, portfolio: Portfolio, order: LimitSell, purchaseFillPriceFn: PriceQuoteFn, saleFillPriceFn: PriceQuoteFn): Boolean =
+    isOrderFillable(trial, portfolio, order.asInstanceOf[MarketSell], purchaseFillPriceFn, saleFillPriceFn) && saleFillPriceFn(order.time, order.symbol) >= order.limitPrice
+
+  def orderFillPrice(order: BuyOrder, purchaseFillPriceFn: PriceQuoteFn, saleFillPriceFn: PriceQuoteFn): BigDecimal = purchaseFillPriceFn(order.time, order.symbol)
+  def orderFillPrice(order: SellOrder, purchaseFillPriceFn: PriceQuoteFn, saleFillPriceFn: PriceQuoteFn): BigDecimal = saleFillPriceFn(order.time, order.symbol)
+
+  def cancelAllPendingOrders(currentState: State): State = currentState.copy(orders = Vector())
+
+  def buy(currentState: State, time: DateTime, symbol: String, qty: Long): State = {
+    val newOrders = currentState.orders :+ MarketBuy(time, symbol, qty, None)
+    currentState.copy(orders = newOrders)
+  }
+
+  def buyImmediately(currentState: State, symbol: String, qty: Long): State = buy(currentState, currentState.time, symbol, qty)
+
+
 }
