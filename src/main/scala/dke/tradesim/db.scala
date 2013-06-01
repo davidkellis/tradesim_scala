72f400d6a500f4db5a06f0a0231433264a41dfc0
David Ellis <davidkellis@gmail.com>
2013-06-01 00:18:45 -0500

    Replaced BigDecimal columns with String columns, and then manually build BigDecimal objects in database access logic

--- src/main/scala/dke/tradesim/db.scala
+++ src/main/scala/dke/tradesim/db.scala
@@ -241,16 +241,16 @@ object db {
 //  }
 
   object SlickAdapter {
-    type EodBarRecord = (Int, String, Timestamp, Long, BigDecimal, BigDecimal, BigDecimal, BigDecimal, Long)
+    type EodBarRecord = (Int, String, Timestamp, Long, String, String, String, String, Long)
     object EodBars extends Table[EodBarRecord]("eod_bars") {
       def id = column[Int]("id", O.PrimaryKey, O.AutoInc)   // This is the primary key column
       def symbol = column[String]("symbol")
       def startTime = column[Timestamp]("start_time")
       def endTime = column[Timestamp]("end_time")
-      def open = column[BigDecimal]("open")
-      def high = column[BigDecimal]("high")
-      def low = column[BigDecimal]("low")
-      def close = column[BigDecimal]("close")
+      def open = column[String]("open")
+      def high = column[String]("high")
+      def low = column[String]("low")
+      def close = column[String]("close")
       def volume = column[Long]("volume")
 
       // Every table needs a * projection with the same type as the table's type parameter
@@ -260,14 +260,14 @@ object db {
     def convertEodBarRecord(record: EodBarRecord): EodBar = {
       record match {
         case (_, symbol, startTime, endTime, open, high, low, close, volume) =>
-          EodBar(symbol, datetime(startTime), datetime(endTime), open, high, low, close, volume)
+          EodBar(symbol, datetime(startTime), datetime(endTime), BigDecimal(open), BigDecimal(high), BigDecimal(low), BigDecimal(close), volume)
       }
     }
 
     def convertEodBarRecord(record: Option[EodBarRecord]): Option[EodBar] = record.map(convertEodBarRecord(_))
 
 
-    type CorporateActionRecord = (Int, String, String, Datestamp, Datestamp, Datestamp, Datestamp, BigDecimal)
+    type CorporateActionRecord = (Int, String, String, Datestamp, Datestamp, Datestamp, Datestamp, String)
     object CorporateActions extends Table[CorporateActionRecord]("corporate_actions") {
       def id = column[Int]("id", O.PrimaryKey, O.AutoInc)   // This is the primary key column
       def kind = column[String]("type")                     // either Split or CashDividend
@@ -278,7 +278,7 @@ object db {
       def recordDate = column[Datestamp]("record_date")
       def payableDate = column[Datestamp]("payable_date")
 
-      def ratioOrAmount = column[BigDecimal]("number")      // split ratio OR dividend amount
+      def ratioOrAmount = column[String]("number")      // split ratio OR dividend amount
 
       // Every table needs a * projection with the same type as the table's type parameter
       def * = id ~ kind ~ symbol ~ declarationDate ~ exDate ~ recordDate ~ payableDate ~ ratioOrAmount
@@ -287,9 +287,9 @@ object db {
     def convertCorporateActionRecord(record: CorporateActionRecord): CorporateAction = {
       record match {
         case (_, "Split", symbol, _, exDate, _, _, ratio) =>
-          Split(symbol, datetime(exDate), ratio)
+          Split(symbol, datetime(exDate), BigDecimal(ratio))
         case (_, "CashDividend", symbol, declarationDate, exDate, recordDate, payableDate, amount) =>
-          CashDividend(symbol, datetime(declarationDate), datetime(exDate), datetime(recordDate), datetime(payableDate), amount)
+          CashDividend(symbol, datetime(declarationDate), datetime(exDate), datetime(recordDate), datetime(payableDate), BigDecimal(amount))
       }
     }
 
