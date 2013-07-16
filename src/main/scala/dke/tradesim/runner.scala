f8e09878b4bba021cb83345afae418a80e3840b2
David Ellis <davidkellis@gmail.com>
2013-07-15 21:22:02 -0500

    fixing a bug that resulted in a warning

--- src/main/scala/dke/tradesim/runner.scala
+++ src/main/scala/dke/tradesim/runner.scala
@@ -34,6 +34,7 @@ object Runner {
           case Some("bah3") => buyandhold.scenarios.runMultipleTrials1()
           case Some("bah4") => buyandhold.scenarios.runMultipleTrials2()
           case Some(name) => println(s"Unknown scenario: $name")
+          case _ => println("No scenario was specified.")
         }
       }
     }
