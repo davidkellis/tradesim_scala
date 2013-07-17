0f6c3290b22fbcb182c96524791e67d4c04b5a30
David Ellis <davidkellis@gmail.com>
2013-07-16 20:39:12 -0500

    adding unit tests to scheduleSpec.scala

--- src/test/scala/dke/tradesim/scheduleSpec.scala
+++ src/test/scala/dke/tradesim/scheduleSpec.scala
@@ -32,4 +32,18 @@ class scheduleSpec extends FunSpec {
       assert(days.toList === date(2013, 3, 27) :: date(2013, 3, 28) :: date(2013, 4, 1) :: date(2013, 4, 2) :: Nil)
     }
   }
+
+  describe("nextTradingDay") {
+    it("returns the next trading day in the given trading schedule") {
+      val tradingSchedule = buildTradingSchedule(defaultTradingSchedule _, defaultHolidaySchedule _)
+      val startDate = date(2013, 3, 26)
+      val tradingDay1 = nextTradingDay(startDate, days(1), tradingSchedule)
+      val tradingDay2 = nextTradingDay(tradingDay1, days(1), tradingSchedule)
+      val tradingDay3 = nextTradingDay(tradingDay2, days(1), tradingSchedule)
+      val tradingDay4 = nextTradingDay(tradingDay3, days(1), tradingSchedule)
+      val tradingDays = List(tradingDay1, tradingDay2, tradingDay3, tradingDay4)
+      val expectedTradingDays = date(2013, 3, 27) :: date(2013, 3, 28) :: date(2013, 4, 1) :: date(2013, 4, 2) :: Nil   // 3/29/2013 is Good Friday
+      assert(tradingDays === expectedTradingDays)
+    }
+  }
 }
