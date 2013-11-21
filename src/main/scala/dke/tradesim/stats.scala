de33752cbb32bda642750e07f5d3e4e74815ce83
racker <noreply@dcx.rackspace.com>
2013-11-21 17:25:58 -0600

    adding the start of a stats library

---
+++ src/main/scala/dke/tradesim/stats.scala
@@ -0,0 +1,8 @@
+package dke.tradesim
+
+object stats {
+  // returns the sample correlation coefficient (Pearson's r coefficient)
+  def correlation(series1: Seq[BigDecimal], series2: Seq[BigDecimal]): BigDecimal = {
+
+  }
+}
