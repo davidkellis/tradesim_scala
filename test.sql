98ae4df980c67a0f67e97494d8814f548d4f9f49
David Ellis <davidkellis@gmail.com>
2014-01-12 00:05:24 -0600

    adding some handy sql for testing

--- test.sql
+++ test.sql
@@ -1,9 +1,17 @@
-﻿select * from trials;
+﻿select * from corporate_actions
+select * from securities where id = 284
+select * from exchanges where id = 1957
+select * from strategies
+select * from trial_sets
+select * from securities_trial_sets
+select * from trials limit 1
 delete from trials;
 
 select *, 
        (cast(high as double precision) + cast(low as double precision))/2.0 simprice,
        (cast(high as double precision) + cast(low as double precision))/2.0 * 376 simtotal,
-       (cast(high as double precision) + cast(low as double precision))/2.0 + 0.3 * (cast(high as double precision) - (cast(high as double precision) + cast(low as double precision))/2.0) fillprice,
-       ((cast(high as double precision) + cast(low as double precision))/2.0 + 0.3 * (cast(high as double precision) - (cast(high as double precision) + cast(low as double precision))/2.0)) * 376 + 7 filltotal
-from eod_bars where security_id = 27136 order by start_time limit 10;
+       (cast(high as double precision) + cast(low as double precision))/2.0 + 0.3 * (cast(high as double precision) - (cast(high as double precision) + cast(low as double precision))/2.0) buyfillprice,
+       ((cast(high as double precision) + cast(low as double precision))/2.0 + 0.3 * (cast(high as double precision) - (cast(high as double precision) + cast(low as double precision))/2.0)) * 376 + 7 buyfilltotal,
+       (cast(high as double precision) + cast(low as double precision))/2.0 + 0.3 * (cast(low as double precision) - (cast(high as double precision) + cast(low as double precision))/2.0) sellfillprice,
+       ((cast(high as double precision) + cast(low as double precision))/2.0 + 0.3 * (cast(low as double precision) - (cast(high as double precision) + cast(low as double precision))/2.0)) * 376 - 7 sellfilltotal
+from eod_bars where security_id = 284 order by start_time limit 260;
